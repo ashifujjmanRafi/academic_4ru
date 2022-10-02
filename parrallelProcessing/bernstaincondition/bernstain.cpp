0c9d0b1fb02a5fe1ffa2c636b2626efca6ba3a81
ashifujjmanRafi <ajrafi2722@gmail.com>
2022-10-02 13:27:02 +0600

    state diagram added

--- parrallelProcessing/bernstaincondition/bernstain.cpp
+++ parrallelProcessing/bernstaincondition/bernstain.cpp
@@ -1,14 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main()
-{
+int main(){
 
     freopen("input.txt", "r", stdin);
     freopen("outpu.txt", "w", stdout);
     string s;
     vector<vector<char>> in;
     vector<char> out;
+
+    
 
     while (getline(cin, s))
     {
@@ -32,6 +33,7 @@ int main()
         }
         in.push_back(temp);
     }
+    vector<vector<string>> dia(out.size(),vector<string>(out.size()));
 
     // for (auto i : out)
     //     cout << i << " ";
@@ -52,21 +54,30 @@ int main()
 
             //first condition 
             if(find(in[i].begin(),in[i].end(),out[j]) != in[i].end()){
-                cout<<"P"<<i+1<<" to "<<"P"<<j+1<<" :P"<<j+1<<" is antidependent to P"<<i+1<<endl;
+                //cout<<"P"<<i+1<<" to "<<"P"<<j+1<<" :P"<<j+1<<" is antidependent to P"<<i+1<<endl;
+                dia[i][j] = "P"+to_string(j+1)+" is antidependent to P"+to_string(i+1);
                 continue;
+                
             }
             if(find(in[j].begin(),in[j].end(),out[i]) != in[j].end()){
-                cout<<"P"<<i+1<<" to "<<"P"<<j+1<<" :P"<<j+1<<" is followdependent to P"<<i+1<<endl;
+                //cout<<"P"<<i+1<<" to "<<"P"<<j+1<<" :P"<<j+1<<" is followdependent to P"<<i+1<<endl;
+                dia[i][j] = "P"+to_string(j+1)+" is followdependent to P"+to_string(i+1);
                 continue;
             }
             if(out[i]==out[j]){
-                cout<<"P"<<i+1<<" to "<<"P"<<j+1<<" :P"<<j+1<<" is outputdependent to P"<<i+1<<endl;
+                //cout<<"P"<<i+1<<" to "<<"P"<<j+1<<" :P"<<j+1<<" is outputdependent to P"<<i+1<<endl;
+                dia[i][j] = "P"+to_string(j+1)+" is outputdependent to P"+to_string(i+1);
                 continue;
             }
-            cout<<"P"<<i+1<<" to "<<"P"<<j+1<<" :P"<<i+1<<",P"<<j+1<<" are independent"<<endl;
+            //cout<<"P"<<i+1<<" to "<<"P"<<j+1<<" :P"<<i+1<<",P"<<j+1<<" are independent"<<endl;
+            dia[i][j] = "P"+to_string(i+1)+",P"+to_string(j+1)+" are independent";
             cout<<endl;
+
+            cout<<dia[i][j]<<endl;
         }
     }
+
+    
 
     return 0;
 }
