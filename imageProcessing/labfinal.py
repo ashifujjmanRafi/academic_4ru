0d5d3dc2e92e12a5aff7f7c1f1de1bb252fb6600
ashifujjmanRafi <ajrafi2722@gmail.com>
2022-11-19 00:44:35 +0600

    final lab practice

--- imageProcessing/labfinal.py
+++ imageProcessing/labfinal.py
@@ -2,20 +2,7 @@ import matplotlib.pyplot as plt
 import cv2 
 import numpy as np
 
-def convulation(img,kernal):
-    r,c = img.shape
-    x,y = kernal.shape
-    out = np.zeros((r-x-1,c-y-1))
-    for i in range(r-x-1):
-        for j in range(c-y-1):
-            x = np.sum(np.multiply(img[i:i+3,j:j+3],kernal))
-            if(x<0):
-                out[i][j]=0
-            elif(x>255):
-                out[i][j]=255
-            else:
-                out[i][j]=x
-    return out
+
 def hist(img):
     d = dict()
     for i in range(256):
@@ -25,25 +12,35 @@ def hist(img):
             d[j]+= 1
     return d
             
-
-
 def main():
     img_path = "mountain.jpeg"
     rgb = plt.imread(img_path)
     gray = cv2.cvtColor(rgb,cv2.COLOR_RGB2GRAY)
     _,binary = cv2.threshold(gray,126,255,cv2.THRESH_BINARY)
-    #imginhist(rgb,gray,binary)
-    #assignment2(gray)
-    #assignment3(gray)
-    #bitslici_masking(gray)
-    #addnoise(gray)
-    #shifting(gray)
-    #morphological(binary)
-    #histogramequalization(gray)
+    # histshow(rgb,gray,binary)
+    # pointprocessing(gray)
+    # neighbourprocessing(gray)
+    # bitslici_masking(gray)
+    # addnoise(gray)
+    # shifting(gray)
+    # morphological(binary)
+    # histogramequalization(gray)
+    # frequencydomain(gray)
 
+def imshow(img_set,x,y):
+    for i in range(len(img_set)):
+        plt.subplot(x,y,i+1)
+        plt.imshow(img_set[i],'gray')
+    plt.savefig('result')
+    plt.show()
 
+def plotshow(plt_list,x,y):
+    for i in range(len(plt_list)):
+        plt.subplot(x,y,i+1)
+        plt.plot(plt_list[i],'red')
+    plt.show()
 
-def imginhist(rgb,gray,binary):
+def histshow(rgb,gray,binary):
 
     r,c = gray.shape
     binary2 = gray.copy()
@@ -68,13 +65,7 @@ def imginhist(rgb,gray,binary):
     imshow(img_set,4,2)
     plotshow(hist_list,3,2)
 
-def imshow(img_set,x,y):
-    for i in range(len(img_set)):
-        plt.subplot(x,y,i+1)
-        plt.imshow(img_set[i],'gray')
-    plt.show()
-
-def assignment2(img):
+def pointprocessing(img):
     r,c = img.shape
     t1,t2 = 100,140
     #first condition:s = 100, if r >= T1 and r <= T2; otherwise s = 10.
@@ -100,20 +91,31 @@ def assignment2(img):
     img_set = [img,c1,c2,c3,c4]
     imshow(img_set,3,2)
     
-def assignment3(img):
-
+def neighbourprocessing(img):
+    #filter
     hor = np.array([[-2,-2,-2],[0,0,0],[2,2,2]])
     ver = np.array([[2,0,-2],[2,0,-2],[2,0,-2]])
     lapla = np.array([[0,-1,0],[-1,5,-1],[0,-1,0]])
     blur = np.ones((3,3))/9
     edge = np.array([[-1,-1,-1],[-1,10,-1],[-1,-1,-1]])
-
+    #convulation operation
     h = cv2.filter2D(img,-1,hor)
     v = cv2.filter2D(img,-1,ver)
     lap = cv2.filter2D(img,-1,lapla)
-    #custom hist custom neighbour processing
-    blurim = convulation(img,blur)
-    e = convulation(img,edge)
+    blurim = cv2.filter2D(img,-1,blur)
+    #custom convulation
+    r,c = img.shape
+    x,y = edge.shape
+    e = np.zeros((r-x-1,c-y-1),dtype=np.uint8)
+    for i in range(r-x-1):
+        for j in range(c-y-1):
+            sum = np.sum(np.multiply(img[i:i+x,j:j+y],edge))
+            if(sum<0):
+                e[i][j]=0
+            elif(sum>255):
+                e[i][j]=255
+            else:
+                e[i][j]=sum
 
     img_set = [h,v,lap,blurim,e]
     imshow(img_set,2,3)
@@ -147,12 +149,6 @@ def addnoise(x):
             img[x][y]=0
     plt.subplot(1,2,1)
     plt.imshow(img,'gray')
-    plt.show()
-
-def plotshow(plt_list,x,y):
-    for i in range(len(plt_list)):
-        plt.subplot(x,y,i+1)
-        plt.plot(plt_list[i],'red')
     plt.show()
 
 def shifting(img):
@@ -205,5 +201,74 @@ def histogramequalization(img):
     e_hist = cv2.calcHist([equlize_hist],[0],None,[256],[0,256])
     plt_list=[n_hist,e_hist]
     plotshow(plt_list,1,2)
+
+def frequencydomain(img):
+
+    r,c = img.shape
+    #fd = frequency domain
+    #fds = centered frequency
+    #h = filter
+    #fdsh = filtered in frequency domain
+    #fdh = invert shifted
+    #sdh = invert in spatial domain
+    fd = np.fft.fft2(img)
+    fds = np.fft.fftshift(fd)
+    #for printing fds_abs=np.log1p(np.abs(fds))
+    #making low pass filter
+
+    #butterworth filter
+    h = np.zeros((r,c),dtype=np.float32)
+    d0 = 20
+    for u in range(r):
+        for v in range(c):
+            d = np.sqrt((u-r/2)**2+(v-c/2)**2)
+            h[u,v]=1/(1+(d/d0)**2)
+    #for printing 
+    h_abs=np.log1p(np.abs(h))
+    #filtered in frequency domain
+    fdsh = fds*h_abs
+    #for printing fdsh_abs=np.log1p(np.abs(fdsh))
+    #inverse in spatial domain
+    #inverse shifting
+    fdh = np.fft.ifftshift(fdsh)
+    #spatial domain
+    sdh = np.abs(np.fft.ifft2(fdh))
+
+    #high pass filter
+    hp = 1 - h
+    hp_abs = np.log1p(np.abs(hp))
+    #filtered in frequency domain
+    fdshp =  fds*hp
+    fdhp = np.fft.ifftshift(fdshp)
+    sdhp = np.abs(np.fft.ifft2(fdhp))
+
+    img_set = [img,h_abs,sdh,hp_abs,sdhp]
+    imshow(img_set,2,3)
+
+
+    #gaussain filter
+    h = np.zeros((r,c),dtype=np.float32)
+    d0 = 40
+    for u in range(r):
+        for v in range(c):
+            d = np.sqrt((u-r/2)**2+(v-c/2)**2)
+            h[u,v] = np.exp(-(d**2)/(2*d0*d0))
+
+    h_abs = np.log1p(np.abs(h))
+    fdsh = fds*h
+    fdh = np.fft.ifftshift(fdsh)
+    sdh = np.abs(np.fft.ifft2(fdh))
+    #highpass
+    hp = 1 - h
+    hp_abs = np.log1p(np.abs(hp))
+    fdshp = fds*hp
+    fdhp = np.fft.ifftshift(fdshp)
+    sdhp = np.abs(np.fft.ifft2(fdhp))
+
+    img_set = [img,h_abs,sdh,hp_abs,sdhp]
+    imshow(img_set,2,3)
+
+
+
 if __name__=="__main__":
     main()
