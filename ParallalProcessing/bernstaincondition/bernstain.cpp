2b2d1fc406f5822903845aa75be56f570176309e
ashifujjmanRafi <ajrafi2722@gmail.com>
2022-11-28 20:00:44 +0600

    parallal exam update

--- ParallalProcessing/bernstaincondition/bernstain.cpp
+++ ParallalProcessing/bernstaincondition/bernstain.cpp
@@ -2,82 +2,55 @@
 using namespace std;
 
 int main(){
-
-    freopen("input.txt", "r", stdin);
-    freopen("outpu.txt", "w", stdout);
-    string s;
     vector<vector<char>> in;
     vector<char> out;
+    string s;
+    freopen("input.txt","r",stdin);
 
-    
-
-    while (getline(cin, s))
-    {
-
+    while(getline(cin,s)){
         vector<char> temp;
         bool flag = true;
 
-        for (int i = 1; i < s.size(); i++)
-        {
-
-            if (flag && isalpha(s[i]))
-            {
+        for(int i = 1;i<s.size();i++){
+            if(flag && isalpha(s[i])){
                 out.push_back(s[i]);
                 flag = false;
                 continue;
             }
-            if (!flag && isalpha(s[i]))
-            {
+            else if(!flag && isalpha(s[i])){
                 temp.push_back(s[i]);
             }
         }
         in.push_back(temp);
     }
-    vector<vector<string>> dia(out.size(),vector<string>(out.size()));
-
-    // for (auto i : out)
-    //     cout << i << " ";
-    // cout << endl;
-    // for (auto i : in)
-    // {
-    //     for (auto j : i)
-    //     {
-    //         cout << j << " ";
-    //     }
-    //     cout << endl;
-    // }
-
-    //checking bernstin condition
 
     for(int i = 0;i<in.size();i++){
-        for(int j = i+1 ; j<in.size();j++){
 
-            //first condition 
-            if(find(in[i].begin(),in[i].end(),out[j]) != in[i].end()){
-                //cout<<"P"<<i+1<<" to "<<"P"<<j+1<<" :P"<<j+1<<" is antidependent to P"<<i+1<<endl;
-                dia[i][j] = "P"+to_string(j+1)+" is antidependent to P"+to_string(i+1);
-                continue;
-                
+        for(int j = i+1 ;j<in.size();j++){
+
+            if(find(in[j].begin(),in[j].end(),out[i])!=in[j].end()){
+
+                cout<<"P"<<i+1<<" and P"<<j+1<<" are folow-dependent";
+
             }
-            if(find(in[j].begin(),in[j].end(),out[i]) != in[j].end()){
-                //cout<<"P"<<i+1<<" to "<<"P"<<j+1<<" :P"<<j+1<<" is followdependent to P"<<i+1<<endl;
-                dia[i][j] = "P"+to_string(j+1)+" is followdependent to P"+to_string(i+1);
-                continue;
+            else if(find(in[i].begin(),in[i].end(),out[j])!=in[i].end()){
+
+                cout<<"P"<<i+1<<" and P"<<j+1<<" are anti-dependent";
             }
-            if(out[i]==out[j]){
-                //cout<<"P"<<i+1<<" to "<<"P"<<j+1<<" :P"<<j+1<<" is outputdependent to P"<<i+1<<endl;
-                dia[i][j] = "P"+to_string(j+1)+" is outputdependent to P"+to_string(i+1);
-                continue;
+            else if(out[i]==out[j]){
+
+                cout<<"P"<<i+1<<" and P"<<j+1<<" are output-dependent";
+
             }
-            //cout<<"P"<<i+1<<" to "<<"P"<<j+1<<" :P"<<i+1<<",P"<<j+1<<" are independent"<<endl;
-            dia[i][j] = "P"+to_string(i+1)+",P"+to_string(j+1)+" are independent";
+            else{
+                cout<<"P"<<i+1<<" and P"<<j+1<<" are in-dependent";
+
+            }
             cout<<endl;
 
-            cout<<dia[i][j]<<endl;
         }
     }
 
-    
 
-    return 0;
+return 0;
 }
--- ParallalProcessing/parallal.md
+++ ParallalProcessing/parallal.md
@@ -4,6 +4,7 @@ use sudo apt-get update to update the system
 use sudo apt install openmpi-bin libopenmpi-dev to install mpi
 use echo 0 | sudo tee /proc/sys/kernel/yama/ptrace_scope to resolve some problem (may be only for wsl)
 go to your projects folder
+
 mpicc name.c -o mpi.o (compile c file)
 mpiCC name.cpp -o mpi.o (compile cpp file)
 mpirun -np 4 ./mpi.o (run mpi)
